b32f77dad364725959294ca8547c164fa3abbf49
Jonatan Gonzalez (He/Him) <jongonza@microsoft.com>
2025-04-07 13:25:01 -0700

    remove test tthat isn't used

--- test/NuGet.Core.Tests/NuGet.Packaging.Test/PackageBuilderTest.cs
+++ test/NuGet.Core.Tests/NuGet.Packaging.Test/PackageBuilderTest.cs
@@ -1455,26 +1455,6 @@ namespace NuGet.Packaging.Test
         }
 
         [Fact]
-        public void Save_DuplicateFilesSameSource_DoesNotThrow()
-        {
-            // Arrange
-            var builder = new PackageBuilder
-            {
-                Id = "A",
-                Version = NuGetVersion.Parse("1.0"),
-                Description = "Test",
-            };
-            builder.Authors.Add("Test");
-            builder.Files.Add(new PhysicalPackageFile { SourcePath = @"a\source\one\Foo.dll", TargetPath = @"lib\net5.0\Foo.dll" });
-            builder.Files.Add(new PhysicalPackageFile { SourcePath = @"a\source\one\Foo.dll", TargetPath = @"lib\net5.0\Foo.dll" });
-            builder.Files.Add(new PhysicalPackageFile { TargetPath = @"lib\net5.0\Bar.dll" });
-            builder.Files.Add(new PhysicalPackageFile { TargetPath = @"lib\net5.0\Bar.dll" });
-            builder.Files.Add(new PhysicalPackageFile { TargetPath = @"lib\net5.0\Baz.dll" });
-
-            ExceptionAssert.Throws<PackagingException>(() => builder.Save(new MemoryStream()), $@"Attempted to pack multiple files into the same location(s). The following destinations were used multiple times: lib{Path.DirectorySeparatorChar}net5.0{Path.DirectorySeparatorChar}Foo.dll, lib{Path.DirectorySeparatorChar}net5.0{Path.DirectorySeparatorChar}Bar.dll");
-        }
-
-        [Fact]
         public void SavingPackageValidatesReferences()
         {
             // Arrange
