b652a6afdec86bf0d72cb99f26de6eba1e18720a
Jonatan Gonzalez (He/Him) <jongonza@microsoft.com>
2025-05-05 15:59:41 -0700

    Create a base deprecation capability

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/Package/DeprecationPreloadedCapability.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/Package/DeprecationPreloadedCapability.cs
@@ -3,72 +3,20 @@
 
 #nullable enable
 
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using NuGet.VisualStudio.Internal.Contracts;
 
 namespace NuGet.PackageManagement.UI.Models.Package
 {
-    internal class DeprecationPreloadedCapability : IDeprecationCapable
+    internal class DeprecationPreloadedCapability : DeprecationCapabilityBase
     {
-        private PackageDeprecationMetadataContextInfo _deprecationMetadata;
-
         public DeprecationPreloadedCapability(PackageDeprecationMetadataContextInfo deprecationMetadata)
         {
             _deprecationMetadata = deprecationMetadata;
         }
 
-        public AlternatePackageMetadataContextInfo? AlternatePackage => _deprecationMetadata?.AlternatePackage;
-
-        public bool IsDeprecated => _deprecationMetadata != null;
-
-        public PackageDeprecationReason PackageDeprecationReasons
-        {
-            get
-            {
-                if (_deprecationMetadata?.Reasons == null || _deprecationMetadata.Reasons.Count == 0)
-                {
-                    return PackageDeprecationReason.Unknown;
-                }
-
-                bool hasCriticalBugs = false;
-                bool hasLegacy = false;
-
-                foreach (var reason in _deprecationMetadata.Reasons)
-                {
-                    if (string.Equals(reason, PackageDeprecationReasonConstants.CriticalBugs, StringComparison.OrdinalIgnoreCase))
-                    {
-                        hasCriticalBugs = true;
-                    }
-                    else if (string.Equals(reason, PackageDeprecationReasonConstants.Legacy, StringComparison.OrdinalIgnoreCase))
-                    {
-                        hasLegacy = true;
-                    }
-
-                    if (hasCriticalBugs && hasLegacy)
-                    {
-                        return PackageDeprecationReason.LegacyAndCriticalBugs;
-                    }
-                }
-
-                if (hasCriticalBugs)
-                {
-                    return PackageDeprecationReason.CriticalBugs;
-                }
-
-                if (hasLegacy)
-                {
-                    return PackageDeprecationReason.Legacy;
-                }
-
-                return PackageDeprecationReason.Unknown;
-            }
-        }
-
-        public PackageDeprecationMetadataContextInfo? DeprecationMetadata => _deprecationMetadata;
-
-        public Task PopulateDataAsync(CancellationToken cancellationToken)
+        public override Task PopulateDataAsync(CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
