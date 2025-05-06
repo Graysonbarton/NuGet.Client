df65faadcbf2146829e1f1001ab36d6948dccd38
Jonatan Gonzalez (He/Him) <jongonza@microsoft.com>
2025-05-06 11:03:03 -0700

    Add nullable and copyright

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/Package/IPackageModelFactory.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/Package/IPackageModelFactory.cs
@@ -1,3 +1,8 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#nullable enable
+
 using NuGet.VisualStudio.Internal.Contracts;
 using ContractItemFilter = NuGet.VisualStudio.Internal.Contracts.ItemFilter;
 
